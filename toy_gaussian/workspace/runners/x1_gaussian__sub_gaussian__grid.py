e7537c98572a8169d33e2953af465363ab70e2ce
Richard <richardhayes777@gmail.com>
2019-12-11 09:50:27 +0000

    prepped for grid promise fix

--- toy_gaussian/workspace/runners/x1_gaussian__sub_gaussian__grid.py
+++ toy_gaussian/workspace/runners/x1_gaussian__sub_gaussian__grid.py
@@ -1,26 +1,25 @@
 import os
 
+import autofit as af
+import toy_gaussian as toy
+from toy_gaussian.workspace.pipelines.initialize import x1_gaussian
+from toy_gaussian.workspace.pipelines.main import x1_gaussian__sub_gaussian__grid
+
+
 # Welcome to the pipeline runner. This tool allows you to load dataset on strong lenses and pass it to pipelines for a
 # strong lens analysis. To show you around, we'll load up some example dataset and run it through some of the example
 # pipelines that come distributed with PyAutoLens, in the 'autolens_workspace/pipelines/simple' folder.
-
 # In this runner, our analysis assumes there is a lens light component that requires modeling and subtraction.
-
 # Why is this folder called 'simple'? Well, we actually recommend you break pipelines up. Details of how to do this
 # can be found in the 'autolens_workspace/runners/advanced' folder, but its a bit conceptually difficult. So, to
 # familiarize yourself with PyAutoLens, I'd stick to the simple pipelines for now!
-
 # You should also checkout the 'autolens_workspace/runners/features' folder. Here, you'll find features that are
 # available in pipelines, that allow you to customize the analysis.
-
 # This runner is supplied as both this Python script and a Juypter notebook. Its up to you which you use - I personally
 # prefer the python script as provided you keep it relatively small, its quick and easy to comment out different lens
 # names and pipelines to set off different analyses. However, notebooks are a tidier way to manage visualization - so
 # feel free to use notebooks. Or, use both for a bit, and decide your favourite!
-
 ### AUTOFIT + CONFIG SETUP ###
-
-import autofit as af
 
 # Setup the path to the autolens_workspace, using a relative directory name.
 workspace_path = "{}/../".format(os.path.dirname(os.path.realpath(__file__)))
@@ -35,7 +34,6 @@ af.conf.instance = af.conf.Config(
 
 ### AUTOLENS + DATA SETUP ###
 
-import toy_gaussian as toy
 
 # It is convenient to specify the lens name as a string, so that if the pipeline is applied to multiple images we \
 # don't have to change all of the path entries in the function below.
@@ -55,28 +53,18 @@ imaging = toy.imaging.from_fits(
     pixel_scales=pixel_scales,
 )
 
-toy.plot.imaging.subplot(imaging=imaging)
+# toy.plot.imaging.subplot(imaging=imaging)
 
 # Running a pipeline is easy, we simply import it from the pipelines folder and pass the lens dataset to its run function.
 # Below, we'll use a 3 phase example pipeline to fit the dataset with a parametric lens light, mass and source light
 # profile. Checkout autolens_workspace/pipelines/examples/lens_sersic_sie_shear_source_sersic.py_' for a full
 # description of the pipeline.
 
-from toy_gaussian.workspace.pipelines.initialize import x1_gaussian
 
 pipeline_initialize = x1_gaussian.make_pipeline(
     phase_folders=["gaussian_x1__sub_gaussian__grid", dataset_label]
 )
 
-from toy_gaussian.workspace.pipelines.main import x1_gaussian__sub_gaussian__grid
-
-pipeline_main = x1_gaussian__sub_gaussian__grid.make_pipeline(
-    phase_folders=["gaussian_x1__sub_gaussian__grid", dataset_label], parallel=True
-)
-
-pipeline = pipeline_initialize + pipeline_main
-
-pipeline.run(dataset=imaging)
 
 # Another example pipeline is shown below, which fits the dataset using a pixelized inversion for the source light.
 # If you commented out the 3 lines of code above, and uncomment the code below, you can easily set this pipeline
@@ -85,3 +73,12 @@ pipeline.run(dataset=imaging)
 # from autolens_workspace.pipelines.simple import lens_sersic_sie_shear_source_inversion
 # pipeline = lens_sersic_sie_shear_source_inversion.make_pipeline(phase_folders=[dataset_label, dataset_name])
 # pipeline.run(dataset_label=imaging)
+
+if __name__ == "__main__":
+    pipeline_main = x1_gaussian__sub_gaussian__grid.make_pipeline(
+        phase_folders=["gaussian_x1__sub_gaussian__grid", dataset_label], parallel=False
+    )
+
+    pipeline = pipeline_initialize + pipeline_main
+
+    pipeline.run(dataset=imaging)
