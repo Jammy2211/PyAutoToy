244af1c764753726e891707a432410bc3dd7c647
Richard <richardhayes777@gmail.com>
2020-01-10 11:22:09 +0000

    trivial interaction matrix

--- time_series/test_time_series/test_species.py
+++ time_series/test_time_series/test_species.py
@@ -1,10 +1,26 @@
+import numpy as np
+import pytest
+
 from time_series import species as s
 
 
-class TestCase:
+@pytest.fixture(name="species")
+def make_species():
+    return s.Species()
+
+
+class TestSpecies:
     def test_interaction(self):
         species = s.Species()
         assert species.interactions[species] == 1.0
 
         species.interactions[species] = 0.5
         assert species.interactions[species] == 0.5
+
+
+class TestSpeciesCollection:
+    def test_interaction_matrix(self, species):
+        collection = s.SpeciesCollection(species)
+
+        assert collection.species == (species,)
+        assert collection.interaction_matrix == np.array([[1.0]])
--- time_series/time_series/species.py
+++ time_series/time_series/species.py
@@ -1,6 +1,24 @@
 from collections import defaultdict
 
+import numpy as np
+
 
 class Species:
     def __init__(self):
         self.interactions = defaultdict(lambda: 1.0)
+
+
+class SpeciesCollection:
+    def __init__(self, *species):
+        self.species = species
+        for species_a in self.species:
+            for species_b in self.species:
+                if species_b not in species_a.interactions:
+                    species_a.interactions[species_b] = 1.0
+
+    @property
+    def interaction_matrix(self):
+        return np.array([
+            list(species.interactions.values())
+            for species in self.species
+        ])
