ddee0196752d7a909b5ef6bde508c6f21cd455f8
Richard <richardhayes777@gmail.com>
2020-01-10 16:12:30 +0000

    docs and types

--- time_series/time_series/matrix_prior_model.py
+++ time_series/time_series/matrix_prior_model.py
@@ -5,11 +5,35 @@ from time_series.species import Matrix
 
 
 class MatrixPriorModel(af.CollectionPriorModel, Matrix):
-    def __init__(self, cls, *items):
-        super().__init__(*items)
+    def __init__(self, cls: type, items: list):
+        """
+        A collection prior model with a custom class and convenience
+        methods for describing interactions between the members.
+
+        Parameters
+        ----------
+        cls
+            A class that implements the Matrix interface
+        items
+            A collection of items that implement the Species interface
+        """
+        super().__init__(items)
         self.cls = cls
 
-    def instance_for_arguments(self, arguments):
+    def instance_for_arguments(self, arguments: dict) -> object:
+        """
+        Create an instance of the class with a ModelInstance created from the set of items
+        as the only argument.
+
+        Parameters
+        ----------
+        arguments
+            A dictionary mapping Priors to physical values
+
+        Returns
+        -------
+        An instance of self.cls
+        """
         species = super().instance_for_arguments(
             arguments
         )
@@ -21,6 +45,10 @@ class MatrixPriorModel(af.CollectionPriorModel, Matrix):
         )
 
     def __setattr__(self, key, value):
+        """
+        By default the CollectionPriorModel wraps any attached object in a PriorModel - here
+        we want to keep the underlying class object for use in instantiation.
+        """
         if key == "cls":
             object.__setattr__(self, key, value)
         else:
