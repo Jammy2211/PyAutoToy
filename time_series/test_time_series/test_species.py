ddfb417db4ff399d7d06beb094e1cfd2a4aff8ba
Richard <richardhayes777@gmail.com>
2020-01-10 12:22:51 +0000

    sophisticated indexing

--- time_series/test_time_series/test_species.py
+++ time_series/test_time_series/test_species.py
@@ -37,7 +37,7 @@ class TestSpeciesCollection:
     def test_trivial_interaction_matrix(self, species_a):
         collection = s.SpeciesCollection(species_a)
 
-        assert collection.species == (species_a,)
+        assert collection.species == [species_a]
         assert collection.interaction_matrix == np.array([[1.0]])
 
     def test_interaction_matrix(
@@ -72,3 +72,30 @@ class TestSpeciesCollection:
     ):
         assert species_collection[0] == species_a
         assert species_collection[1] == species_b
+
+        species_collection[0] = species_b
+        species_collection[1] = species_a
+
+        assert species_collection[0] == species_b
+        assert species_collection[1] == species_a
+
+    def test_interaction_indexing(
+            self,
+            species_a,
+            species_b,
+            species_collection
+    ):
+        species_collection[0, 0] = 0.0
+        species_collection[1, 0] = 1.0
+        species_collection[0, 1] = 0.1
+        species_collection[1, 1] = 1.1
+
+        assert species_a[species_a] == 0.0
+        assert species_b[species_a] == 1.0
+        assert species_a[species_b] == 0.1
+        assert species_b[species_b] == 1.1
+
+        assert species_collection[0, 0] == 0.0
+        assert species_collection[1, 0] == 1.0
+        assert species_collection[0, 1] == 0.1
+        assert species_collection[1, 1] == 1.1
--- time_series/time_series/species.py
+++ time_series/time_series/species.py
@@ -8,10 +8,16 @@ class Species:
         self.interactions = defaultdict(lambda: 1.0)
         self.growth_rate = growth_rate
 
+    def __getitem__(self, item):
+        return self.interactions[item]
+
+    def __setitem__(self, key, value):
+        self.interactions[key] = value
+
 
 class SpeciesCollection:
     def __init__(self, *species):
-        self.species = species
+        self.species = list(species)
         for species_a in self.species:
             for species_b in self.species:
                 if species_b not in species_a.interactions:
@@ -37,4 +43,13 @@ class SpeciesCollection:
         ])
 
     def __getitem__(self, item):
-        return self.species[item]
+        if isinstance(item, int):
+            return self.species[item]
+        elif isinstance(item, tuple):
+            return self[item[0]][self[item[1]]]
+
+    def __setitem__(self, key, value):
+        if isinstance(key, int):
+            self.species[key] = value
+        elif isinstance(key, tuple):
+            self[key[0]][self[key[1]]] = value
