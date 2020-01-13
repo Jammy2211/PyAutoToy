f0fad59151fdebcac3309ebc7ff26961f838ab36
Richard <richardhayes777@gmail.com>
2020-01-13 14:29:32 +0000

    docs and types

--- time_series/time_series/observable.py
+++ time_series/time_series/observable.py
@@ -5,14 +5,27 @@ from scipy import stats
 class Observable:
     def __init__(
             self,
-            mean,
-            deviation
+            mean: float,
+            deviation: float
     ):
+        """
+        An observable feature associated with one species which has a Gaussian distribution for that species.
+
+        Parameters
+        ----------
+        mean
+            The mean of the distribution
+        deviation
+            The standard deviation of the distribution
+        """
         self.mean = mean
         self.deviation = deviation
 
     @property
     def distribution(self):
+        """
+        A function for sampling a normal distribution
+        """
         return stats.norm(
             loc=self.mean,
             scale=self.deviation
@@ -20,10 +33,24 @@ class Observable:
 
     def pdf(
             self,
-            lower_limit=-2,
-            upper_limit=2,
-            number_points=1000
-    ):
+            lower_limit: int = -2,
+            upper_limit: int = 2,
+            number_points: int = 1000
+    ) -> np.ndarray:
+        """
+        Generate a numpy array describing the point density function with a given number of points
+        between two limits.
+
+        Parameters
+        ----------
+        lower_limit
+        upper_limit
+        number_points
+
+        Returns
+        -------
+        An array illustrating the point density
+        """
         return self.distribution.pdf(
             np.linspace(
                 lower_limit,
