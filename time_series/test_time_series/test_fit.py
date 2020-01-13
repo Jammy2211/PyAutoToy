43951eeaeabfd87718d6428fdfd2ee2bdcf1dc5b
Richard <richardhayes777@gmail.com>
2020-01-13 14:52:42 +0000

    chi squared

--- time_series/test_time_series/test_fit.py
+++ time_series/test_time_series/test_fit.py
@@ -1,19 +1,31 @@
 import numpy as np
+import pytest
 
 from time_series.fit import Fit
 
 
-class TestFit:
-    def test_residuals(self):
-        fit = Fit(
-            np.array([
-                0.0, 1.0, 2.0
-            ]),
-            np.array([
-                1.0, 1.0, 1.0
-            ])
-        )
+@pytest.fixture(name="fit")
+def make_fit():
+    return Fit(
+        np.array([
+            0.0, 1.0, 2.0
+        ]),
+        np.array([
+            1.0, 1.0, 1.0
+        ])
+    )
 
+
+class TestFit:
+    def test_residuals(self, fit):
         assert (fit.residuals == np.array([
             1.0, 0.0, -1.0
         ])).all()
+
+    def test_chi_squared_list(self, fit):
+        assert (fit.chi_squared_list == np.array([
+            1.0, 0.0, 1.0
+        ])).all()
+
+    def test_chi_squared(self, fit):
+        assert fit.chi_squared == 2.0
--- time_series/time_series/fit.py
+++ time_series/time_series/fit.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 class Fit:
     def __init__(self, model_data, observed_data):
         self.model_data = model_data
@@ -6,3 +9,13 @@ class Fit:
     @property
     def residuals(self):
         return self.observed_data - self.model_data
+
+    @property
+    def chi_squared_list(self):
+        return np.square(
+            self.residuals
+        )
+
+    @property
+    def chi_squared(self):
+        return np.sum(self.chi_squared_list)
