e7a3c8b96499af3444d6bf9f58077368cb8f6756
Richard <richardhayes777@gmail.com>
2020-01-23 15:35:08 +0000

    attempting to run a phase

--- time_series/workspace/pipeline.py
+++ time_series/workspace/pipeline.py
@@ -1,3 +1,5 @@
+import json
+
 import autofit as af
 import time_series as ts
 
@@ -6,13 +8,39 @@ NUMBER_OF_OBSERVABLES = 3
 
 
 def run_phase():
-    model = af.ModelMapper()
     data = ts.generate_data(
         number_of_observables=NUMBER_OF_OBSERVABLES,
         number_of_species=NUMBER_OF_SPECIES
     )
 
+    model = af.ModelMapper()
+    model.abundances = [
+        af.UniformPrior(
+            0, 1
+        )
+        for _ in range(
+            NUMBER_OF_SPECIES
+        )
+    ]
+    model.species = [
+        ts.Species(
+            observables={
+                str(number): af.PriorModel(
+                    ts.Observable,
+                    mean=af.GaussianPrior(3, 1),
+                    deviation=af.GaussianPrior(2, 1)
+                )
+                for number in range(NUMBER_OF_OBSERVABLES)
+            }
+        )
+        for _ in range(NUMBER_OF_SPECIES)
+    ]
+
+    # 5 * 3 * 2 priors for observables + 5 priors for abundances
+    assert model.prior_count == 35
+
     phase = af.Phase(
+        phase_name="Species",
         analysis_class=ts.Analysis,
         model=model
     )
@@ -24,4 +52,4 @@ def run_phase():
 
 
 if __name__ == "__main__":
-    pass
+    run_phase()
